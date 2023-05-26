c50f70343f2ab9b795aeb53adb5f573be0ce2738
Teddy Knox <teddyknox@gmail.com>
2023-05-26 00:55:07 -0400

    Add duality-1 initial RS testnet registration

---
+++ replicated-security/duality-1/start-proposal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export ACCOUNT="<TBD>"
+gaiad tx gov submit-proposal consumer-addition proposal-start-duality-1.json \
+--from=$ACCOUNT \
+--keyring-backend test \
+--chain-id=provider-1 \
+--gas-prices 0.0025uatom --gas-adjustment 1.2 \
+-y
