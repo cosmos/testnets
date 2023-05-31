4d83b34af6200d958cc5a1a40489577be67f5690
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-05-31 09:25:29 -0400

    fix provider chain id

--- replicated-security/duality-rehearsal-1/start-proposal.sh
+++ replicated-security/duality-rehearsal-1/start-proposal.sh
@@ -3,6 +3,6 @@ export ACCOUNT="<TBD>"
 gaiad tx gov submit-proposal consumer-addition proposal-start-duality-rehearsal-1.json \
 --from=$ACCOUNT \
 --keyring-backend test \
---chain-id=provider-1 \
+--chain-id=provider \
 --gas-prices 0.0025uatom --gas-adjustment 1.2 \
 -y
