f3074bf5be7ae02fad66d94ff3a45baf4bb31f82
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-08-27 20:33:48 -0400

    update peers, config, and cv version

--- interchain-security/dungeontest-1/join-rs-dungeon.sh
+++ interchain-security/dungeontest-1/join-rs-dungeon.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-set -ex
-
-# Set up a service to join the slasher chain.
+# Set up a service to join the dungeontest-1 chain.
 
 # Configuration
 # You should only have to modify the values in this block
@@ -18,7 +16,7 @@ SERVICE_NAME=dungeonchain
 
 CHAIN_BINARY='dungeond'
 CHAIN_ID=dungeontest-1
-PERSISTENT_PEERS=“9c9affab7652c9c5a2898d0dd244840c90bc8ba9@dungeon-cherry.ics-testnet.polypore.xyz:26656,4c9fe209358b0821fe48be7e449a3ed02af7b740@dungeon.ics-testnet.polypore.xyz:26656”
+PERSISTENT_PEERS="9c9affab7652c9c5a2898d0dd244840c90bc8ba9@dungeon-cherry.ics-testnet.polypore.xyz:26656,4c9fe209358b0821fe48be7e449a3ed02af7b740@dungeon.ics-testnet.polypore.xyz:26656"
 
 # The genesis file that includes the CCV state will not be published until after the spawn time has been reached.
 GENESIS_URL=https://github.com/cosmos/testnets/raw/master/interchain-security/dungeontest-1/genesis.json.tar.gz
@@ -49,11 +47,10 @@ export PATH=$PATH:$HOME/go/bin
 # Initialize home directory
 echo "Initializing $NODE_HOME..."
 rm -rf $NODE_HOME
-$CHAIN_BINARY config chain-id $CHAIN_ID --home $NODE_HOME
-$CHAIN_BINARY config keyring-backend test --home $NODE_HOME
-$CHAIN_BINARY config broadcast-mode block --home $NODE_HOME
+$CHAIN_BINARY config set client chain-id $CHAIN_ID --home $NODE_HOME
+$CHAIN_BINARY config set client keyring-backend test --home $NODE_HOME
 $CHAIN_BINARY init $NODE_MONIKER --chain-id $CHAIN_ID --home $NODE_HOME
-sed -i -e "/persistent_peers =/ s^= .*^= \"$PERSISTENT_PEERS\"^" $NODE_HOME/config/config.toml
+sed -i -e "s/persistent_peers = \"\"/persistent_peers = \"$PERSISTENT_PEERS\"/" $NODE_HOME/config/config.toml
 
 # Replace keys
 echo "Replacing keys..."
@@ -61,24 +58,23 @@ cp $PRIV_VALIDATOR_KEY_FILE $NODE_HOME/config/priv_validator_key.json
 cp $NODE_KEY_FILE $NODE_HOME/config/node_key.json
 
 # Replace genesis file: only after the spawn time is reached
-echo "Replacing genesis file..."
-wget $GENESIS_URL -O genesis.json.tar.gz
-tar -xvzf genesis.json.tar.gz "./network/dungeontest-1/genesis.json" --one-top-level=genesis.json --strip-components 4
-mv genesis.json $NODE_HOME/config/genesis.json
+# echo "Replacing genesis file..."
+# wget $GENESIS_URL -O genesis.json.tar.gz
+# tar -xvzf genesis.json.tar.gz "./network/dungeontest-1/genesis.json" --one-top-level=genesis.json --strip-components 4
+# mv genesis.json $NODE_HOME/config/genesis.json
 
 echo "Creating $SERVICE_NAME.service..."
 sudo rm /etc/systemd/system/$SERVICE_NAME.service
 sudo touch /etc/systemd/system/$SERVICE_NAME.service
 
 echo "[Unit]"                               | sudo tee /etc/systemd/system/$SERVICE_NAME.service
-echo "Description=slasher service"       | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "Description=dungeon service"       | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
 echo "After=network-online.target"          | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
 echo ""                                     | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
 echo "[Service]"                            | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
 echo "User=$USER"                            | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
 echo "ExecStart=$HOME/go/bin/$CHAIN_BINARY start --x-crisis-skip-assert-invariants --home $NODE_HOME" | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
-echo "Restart=always"                       | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
-echo "RestartSec=3"                         | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "Restart=no"                       | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
 echo "LimitNOFILE=4096"                     | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
 echo ""                                     | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
 echo "[Install]"                            | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
