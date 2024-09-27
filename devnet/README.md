d40eb0d89939876d7698651b6e7f9f03d0542aa4
ASoTNetworks <dev@aseriesoftubez.com>
2024-09-27 15:09:40 -0400

    added devnet scripts and readme

---
+++ devnet/README.md
@@ -0,0 +1,70 @@
+
+# Devnet Details
+
+The gaia-testnet will be used to test Gaia features before a release is tagged. It was created to mirror the state of mainnet using the fork tool.
+
+The snapshot provided is stopped at the upgrade height and it is awating for the upgrade to be applied by running the same `gaiad` version running on the devnet.
+
+* **Chain-ID**: `cosmoshub-4`
+* **denom**: `uatom`
+* **Current Gaia Version**: Refer to https://files.polypore.xyz/gaia-devnet/ for the current version.
+* **Genesis File:**  [genesis.cosmoshub-4.json.gz](https://github.com/cosmos/mainnet/raw/master/genesis/genesis.cosmoshub-4.json.gz), verify with `sha256sum genesis.cosmoshub-4.json.gz`
+* **Genesis sha256sum**: `7fe946e6bb3c378da546767f4d078585c38f256c8ec17888d71aeee3b7edd5c7`
+
+## Endpoints
+
+### RPC
+
+* `https://rpc.gaia-devnet.polypore.xyz`
+
+### API
+
+* `https://rest.gaia-devnet.polypore.xyz`
+
+### gRPC
+
+* `https://grpc.gaia-devnet.polypore.xyz`
+
+### Persistent Peer
+
+* `0ef1c4cbfe5b93a3e778acbc07fe0384567283d2@gaia-devnet.polypore.xyz:26656`
+
+## Add to Keplr
+
+Use this [jsfiddle](https://jsfiddle.net/uw4ar8qt/2/).
+
+## Block Explorers
+
+* https://explorer.polypore.xyz/gaia-devnet
+
+## Snapshot
+
+* https://files.polypore.xyz/gaia-devnet/
+
+## Faucet
+
+* Visit `faucet.polypore.xyz` to request tokens and check your address balance.
+
+## How to Join
+
+The scripts provided in this repo will install Gaia and optionally set up a Cosmovisor service with the auto-download feature disabled on your machine.
+
+### Bash Script
+
+Run either one of the scripts provided in this repo to join the provider chain:
+* `join-gaia-devnet.sh` will create a `gaiad` service.
+* `join-gaia-devnet-cv.sh` will create a `cosmovisor` service.
+* Both scripts must be run either as root or from a sudoer account.
+* Both scripts will build the current running version from the [Gaia repo](https://github.com/cosmos/gaia/).
+
+#### State Sync
+
+* State sync is unavailable on gaia devnet
+
+#### Sync from snapshot
+
+1. Build, install, and configure the matching running version of gaiad
+2. Download the snapshot https://files.polypore.xyz/gaia-devnet/
+3. Remove existing `data` and `wasm` directories in the ~/.gaia directory
+4. Extract the snapshot in the ~/.gaia directory
+5. Start the `gaiad` binary normally with `gaiad start`
---
+++ devnet/join-gaia-devnet.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+# Set up a Gaia service to join the devnet.
+
+# Configuration
+# You should only have to modify the values in this block
+# * Keys
+#    The private validator key and node key operations are provided in case you have a pre-existing set of keys.
+#    If you want to generate these keys as part of the setup, comment out the "Replace keys" section.
+# * Binary
+#    The binary for the amd64 architecture is set by default.
+#    If you are using a different architecture, visit the gaia releases page for an adequate link.
+#    If you want to build from source, uncomment the "or install from source" section
+# ***
+PRIV_VALIDATOR_KEY_FILE=~/priv_validator_key.json
+NODE_KEY_FILE=~/node_key.json
+NODE_HOME=~/.gaia
+NODE_MONIKER=gaiad-devnet
+SERVICE_NAME=gaiad-devnet
+STATE_SYNC=false # currently unsupported on gaia-devnet
+GAS_PRICE=0.005uatom
+# ***
+
+CHAIN_BINARY='gaiad'
+CHAIN_ID=cosmoshub-4
+GENESIS_URL=https://github.com/cosmos/mainnet/raw/master/genesis/genesis.cosmoshub-4.json.gz
+PERSISTENT_PEERS="0ef1c4cbfe5b93a3e778acbc07fe0384567283d2@gaia-devnet.polypore.xyz:26656"
+SYNC_RPC_1=https://rpc.gaia-devnet.polypore.xyz:443
+SYNC_RPC_2=https://rpc.gaia-devnet.polypore.xyz:443
+SYNC_RPC_SERVERS="$SYNC_RPC_1,$SYNC_RPC_2"
+
+# Install wget and jq
+sudo apt-get install curl jq wget -y
+mkdir -p $HOME/go/bin
+export PATH=$PATH:$HOME/go/bin
+
+# Get current devnet version
+GAIA_VERSION=$(curl -s "https://rpc.gaia-devnet.polypore.xyz/abci_info?" | jq -r '.result.response.version')
+
+# Install Gaia binary
+echo "Installing Gaia..."
+
+# Build from source
+## Install go 1.22.6
+echo "Installing go..."
+rm go*linux-amd64.tar.gz
+wget https://go.dev/dl/go1.22.6.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.6.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+echo "Installing build-essential..."
+sudo apt install build-essential -y
+echo "Installing Gaia..."
+cd $HOME
+rm -rf gaia
+git clone https://github.com/cosmos/gaia.git
+cd gaia
+git checkout $GAIA_VERSION
+make install
+
+# Initialize home directory
+echo "Initializing $NODE_HOME..."
+rm -rf $NODE_HOME
+$CHAIN_BINARY config set client chain-id $CHAIN_ID --home $NODE_HOME
+$CHAIN_BINARY config set client keyring-backend test --home $NODE_HOME
+$CHAIN_BINARY init $NODE_MONIKER --chain-id $CHAIN_ID --home $NODE_HOME
+sed -i -e "/minimum-gas-prices =/ s^= .*^= \"$GAS_PRICE\"^" $NODE_HOME/config/app.toml
+sed -i -e "/^persistent_peers =/ s^= .*^= \"$PERSISTENT_PEERS\"^" $NODE_HOME/config/config.toml
+
+# Replace keys
+echo "Replacing keys and genesis file..."
+cp $PRIV_VALIDATOR_KEY_FILE $NODE_HOME/config/priv_validator_key.json
+cp $NODE_KEY_FILE $NODE_HOME/config/node_key.json
+
+if $STATE_SYNC ; then
+    echo "Configuring state sync..."
+    CURRENT_BLOCK=$(curl -s $SYNC_RPC_1/block | jq -r '.result.block.header.height')
+    TRUST_HEIGHT=$[$CURRENT_BLOCK-1000]
+    TRUST_BLOCK=$(curl -s $SYNC_RPC_1/block\?height\=$TRUST_HEIGHT)
+    TRUST_HASH=$(echo $TRUST_BLOCK | jq -r '.result.block_id.hash')
+    sed -i -e '/enable =/ s/= .*/= true/' $NODE_HOME/config/config.toml
+    sed -i -e '/trust_period =/ s/= .*/= "8h0m0s"/' $NODE_HOME/config/config.toml
+    sed -i -e "/trust_height =/ s/= .*/= $TRUST_HEIGHT/" $NODE_HOME/config/config.toml
+    sed -i -e "/trust_hash =/ s/= .*/= \"$TRUST_HASH\"/" $NODE_HOME/config/config.toml
+    sed -i -e "/rpc_servers =/ s^= .*^= \"$SYNC_RPC_SERVERS\"^" $NODE_HOME/config/config.toml
+else
+    echo "Downloading gaia-devnet archive..."
+    cd $NODE_HOME
+    curl -o - -L https://files.polypore.xyz/gaia-devnet/latest.tar.gz | tar vzx -C .
+    cd $HOME
+fi
+
+# Replace genesis file
+wget $GENESIS_URL -O genesis.json.gz
+gunzip genesis.json.gz
+mv genesis.json $NODE_HOME/config/genesis.json
+
+sudo rm /etc/systemd/system/$SERVICE_NAME.service
+sudo touch /etc/systemd/system/$SERVICE_NAME.service
+
+echo "[Unit]"                               | sudo tee /etc/systemd/system/$SERVICE_NAME.service
+echo "Description=Gaia service"             | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "After=network-online.target"          | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo ""                                     | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "[Service]"                            | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "User=$USER"                           | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "ExecStart=$HOME/go/bin/$CHAIN_BINARY start --x-crisis-skip-assert-invariants --home $NODE_HOME" | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "Restart=no"                           | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "LimitNOFILE=4096"                     | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo ""                                     | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "[Install]"                            | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+echo "WantedBy=multi-user.target"           | sudo tee /etc/systemd/system/$SERVICE_NAME.service -a
+
+# Start service
+echo "Starting $SERVICE_NAME.service..."
+sudo systemctl daemon-reload
+sudo systemctl enable $SERVICE_NAME.service
+sudo systemctl start $SERVICE_NAME.service
+sudo systemctl restart systemd-journald
+
+# Add go and gaiad to the path
+echo "Setting up paths for go and cosmovisor current bin..."
+echo "export PATH=$PATH:/usr/local/go/bin" >> .profile
+
+echo "***********************"
+echo "To see the Gaia log enter:"
+echo "journalctl -fu $SERVICE_NAME.service"
+echo "***********************"
