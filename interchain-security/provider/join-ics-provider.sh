5cebae4d3644a77ce6929731382b344730dfdb59
ASoTNetworks <dev@aseriesoftubez.com>
2025-05-21 16:12:33 +0000

    bump go version

--- interchain-security/provider/join-ics-provider.sh
+++ interchain-security/provider/join-ics-provider.sh
@@ -53,8 +53,8 @@ echo "Installing Gaia..."
 ## Install go 1.23.6
 echo "Installing go..."
 rm go*linux-amd64.tar.gz
-wget https://go.dev/dl/go1.23.6.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.6.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.24.1.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.24.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 echo "Installing build-essential..."
 sudo apt install build-essential -y
