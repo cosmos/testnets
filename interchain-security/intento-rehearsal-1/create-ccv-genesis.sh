ead504b986038bc0dc77565fb911ae0455ce02df
danieljdd <danieljdijkstra@gmail.com>
2025-08-15 13:37:59 +0200

    Add Consumer ID

--- interchain-security/intento-rehearsal-1/create-ccv-genesis.sh
+++ interchain-security/intento-rehearsal-1/create-ccv-genesis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gaiad --node https://rpc.provider-sentry-01.rs-testnet.polypore.xyz:443 query provider consumer-genesis IDDDDDDD -o json > ccv.json
+gaiad --node https://rpc.provider-sentry-01.rs-testnet.polypore.xyz:443 query provider consumer-genesis 148 -o json > ccv.json
 jq -s '.[0].app_state.ccvconsumer = .[1] | .[0]' intento-rehearsal-1-genesis-without-ccv.json ccv.json > intento-rehearsal-1-genesis-with-ccv.json
 rm ccv.json
 sha256sum intento-rehearsal-1-genesis-with-ccv.json
